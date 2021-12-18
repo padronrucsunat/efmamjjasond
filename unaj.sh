313e34d6bcae3f99db461aba9fa260fde2192a50
Carabaya <94078994+Carabaya@users.noreply.github.com>
2021-12-17 20:05:04 -0500

    Update unaj.sh

--- unaj.sh
+++ unaj.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 service tor reload &>/dev/null
-curl --socks5 127.0.0.1:9050 checkip.amazonaws.com
+curl --socks5 127.0.0.1:9050 ip.p3terx.com
 python3 doser2.py -g 'https://unaj.edu.pe/sites/default/files/Res-Concejo-comision-2021/Resolucion_Consejo_CO-610.pdf' -t 1000
