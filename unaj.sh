72d9cd8f08defedeab79fd7f97a2670a8045cd55
Carabaya <94078994+Carabaya@users.noreply.github.com>
2022-07-16 21:20:34 -0500

    Update unaj.sh

--- unaj.sh
+++ unaj.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 service tor reload &>/dev/null
 curl --socks5 127.0.0.1:9050 ip.p3terx.com
-python3 doser2.py -g 'https://unaj.edu.pe/sites/default/files/Res-Concejo-comision-2021/Resolucion_Consejo_CO-610.pdf' -t 1000
+python3 doser2.py -g 'https://unaj.edu.pe/sites/default/files/PROSPECTO%202022-I%20segunda%20fase%20UNAJ.pdf' -t 1000
